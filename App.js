3ebe808b62328f97ec1b44df7221d81d8ea82f6e
yumi-xu <happyleaf90@outlook.com>
2024-09-10 13:47:54 -0700

    added a variable name for the app name and used it in the text editment

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "My app!"
   return (
     <View style={styles.container}>
-      <Text>Welcome to yumi's app!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
