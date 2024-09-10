dc4071b981444a7bbabe29aff477f3f1a04e4115
yumi-xu <happyleaf90@outlook.com>
2024-09-10 14:47:29 -0700

    Add a <TextInput> in App.js and show them in the Text

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
+import { useState } from 'react';
 
 export default function App() {
   const appName = "My app!"
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName}/>
+      <TextInput
+        style={{borderBottomColor: "purple", borderBottomWidth:2}}
+        placeholder="Type here!"
+        autoCorrect={true}
+        keyboardType="default"
+        value={text}
+        onChangeText={newText=>setText(newText)}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
