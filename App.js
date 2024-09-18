0898b2ce1519dac7e9efa613c814df75c3fd3555
yumi-xu <happyleaf90@outlook.com>
2024-09-17 17:46:13 -0700

    format code

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import { SafeAreaView, StyleSheet, Text, View, Button, StatusBar} from 'react-native';
-import React, { useState } from 'react';
-import Header from './Components/Header';
-import Input from './Components/Input';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  StatusBar,
+} from "react-native";
+import React, { useState } from "react";
+import Header from "./Components/Header";
+import Input from "./Components/Input";
 
 export default function App() {
-  const appName = "Welcome to My awesome app"
+  const appName = "Welcome to My awesome app";
 
-  const [inputData, setInputData] = useState('');
+  const [inputData, setInputData] = useState("");
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const handleInputData = (data) => {
     setInputData(data);
     setIsModalVisible(false);
-  }
+  };
 
   const handleCancel = () => {
     setIsModalVisible(false);
@@ -23,69 +30,76 @@ export default function App() {
       <StatusBar style="auto" />
       <View style={styles.topSection}>
         <Text style={styles.headerText}>{appName}</Text>
-        <Button title="Add a goal" onPress={() => setIsModalVisible(true)} color="#1E90FF" />
+        <Button
+          title="Add a goal"
+          onPress={() => setIsModalVisible(true)}
+          color="#1E90FF"
+        />
       </View>
-    
-      <Input autoFocus={true} inputHandler={handleInputData} visible={isModalVisible} onCancel={handleCancel}/>
+
+      <Input
+        autoFocus={true}
+        inputHandler={handleInputData}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+      />
       <View style={styles.bottomSection}>
-        {inputData.length > 0 ? (  
+        {inputData.length > 0 ? (
           <View style={styles.textWrapper}>
             <Text style={styles.inputText}>{inputData}</Text>
           </View>
         ) : null}
-      </View>      
+      </View>
     </SafeAreaView>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-  
+
   topSection: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   headerText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: 'purple',
-    textAlign: 'center',
-    borderColor: 'purple',
+    fontWeight: "bold",
+    color: "purple",
+    textAlign: "center",
+    borderColor: "purple",
     borderWidth: 2,
     padding: 10,
     borderRadius: 10,
   },
 
   bottomSection: {
-    flex: 4,  
-    backgroundColor: '#D8BFD8', 
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    flex: 4,
+    backgroundColor: "#D8BFD8",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
 
   textWrapper: {
-    borderRadius: 5, 
-    backgroundColor: '#E6E6FA',
+    borderRadius: 5,
+    backgroundColor: "#E6E6FA",
     padding: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
-    shadowColor: '#000', 
-    shadowOffset: { width: 0, height: 2 }, 
+    borderColor: "#ccc",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 3,
-    elevation: 3,  
+    elevation: 3,
   },
 
   inputText: {
-    fontSize: 18, 
-    color: 'black',  
-    textAlign: 'center',
+    fontSize: 18,
+    color: "black",
+    textAlign: "center",
   },
-
 });
--- Components/Input.js
+++ Components/Input.js
@@ -1,156 +1,162 @@
-import React, { useState} from 'react';
-import { Modal, StyleSheet, View, Text, TextInput, Button, Alert, Image} from 'react-native'
+import React, { useState } from "react";
+import {
+  Modal,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  Image,
+} from "react-native";
 
-export default function Input({autoFocus, inputHandler, visible, onCancel}) {
-    const [text, setText] = useState('');
-    const [showCharCount, setShowCharCount] = useState(true);  
-    const [message, setMessage] = useState('');
-    const [isConfirmDisabled, setIsConfirmDisabled] = useState(true);
+export default function Input({ autoFocus, inputHandler, visible, onCancel }) {
+  const [text, setText] = useState("");
+  const [showCharCount, setShowCharCount] = useState(true);
+  const [message, setMessage] = useState("");
+  const [isConfirmDisabled, setIsConfirmDisabled] = useState(true);
 
-    //lose focus
-    const handleBlur = () => {
-        setShowCharCount(false);
-        if (text.length >= 3) {
-          setMessage("Thank you");
-        } else {
-          setMessage("Please type more than 3 characters");
-        }
-      };
+  //lose focus
+  const handleBlur = () => {
+    setShowCharCount(false);
+    if (text.length >= 3) {
+      setMessage("Thank you");
+    } else {
+      setMessage("Please type more than 3 characters");
+    }
+  };
 
-    //gain focus
-    const handleFocus = () => {
-        setShowCharCount(true);  
-        setMessage('');  
-    };
+  //gain focus
+  const handleFocus = () => {
+    setShowCharCount(true);
+    setMessage("");
+  };
 
-    const handleConfirm = () => {
-        inputHandler(text); 
-        setText('');   
-    };
+  const handleConfirm = () => {
+    inputHandler(text);
+    setText("");
+  };
 
-    const handleCancel = () => {
-        Alert.alert(
-            "Cancel",
-            "Are you sure you want to cancel?",
-            [
-                {
-                    text: "No",
-                    style: "cancel",
-                },
-                {
-                    text: "Yes",
-                    onPress: () => {
-                        setText('');  
-                        onCancel();  
-                    },
-                },
-            ],
-            { cancelable: true }
-        );
-    };
+  const handleCancel = () => {
+    Alert.alert(
+      "Cancel",
+      "Are you sure you want to cancel?",
+      [
+        {
+          text: "No",
+          style: "cancel",
+        },
+        {
+          text: "Yes",
+          onPress: () => {
+            setText("");
+            onCancel();
+          },
+        },
+      ],
+      { cancelable: true }
+    );
+  };
 
-    const handleChangeText = (newText) => {
-        setText(newText);
-        setIsConfirmDisabled(newText.length < 3);
-    };
+  const handleChangeText = (newText) => {
+    setText(newText);
+    setIsConfirmDisabled(newText.length < 3);
+  };
 
-    return (
-        <Modal visible={visible} 
-        animationType="slide"
-        transparent={false}
-        >
-            <View style={styles.container}>
-            <View style={styles.modalContent}>
-                <Image
-                    source={{ uri: 'https://cdn-icons-png.flaticon.com/512/2617/2617812.png' }}
-                    style={styles.image}
-                    alt="Network image"
-                />
-                <Image
-                    source={require('../assets/arrow.png')}
-                    style={styles.image}
-                    alt="Local image"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Type here!"
-                    autoCorrect={true}
-                    keyboardType="default"
-                    value={text}
-                    onChangeText={handleChangeText} 
-                    onBlur={handleBlur}
-                    onFocus={handleFocus}
-                    autoFocus={autoFocus}
-                />
-            
-                {showCharCount && text.length > 0 && (
-                    <Text>
-                    Character Count: {text.length}
-                    </Text>
-                )}
+  return (
+    <Modal visible={visible} animationType="slide" transparent={false}>
+      <View style={styles.container}>
+        <View style={styles.modalContent}>
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+            }}
+            style={styles.image}
+            alt="Network image"
+          />
+          <Image
+            source={require("../assets/arrow.png")}
+            style={styles.image}
+            alt="Local image"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Type here!"
+            autoCorrect={true}
+            keyboardType="default"
+            value={text}
+            onChangeText={handleChangeText}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            autoFocus={autoFocus}
+          />
 
-                {message.length > 0 && (
-                    <Text>
-                    {message}
-                    </Text>
-                )}
+          {showCharCount && text.length > 0 && (
+            <Text>Character Count: {text.length}</Text>
+          )}
 
-                <View style={styles.buttonContainer}>
-                        <Button title="Cancel" onPress={handleCancel} color="#1E90FF" />
-                        <Button title="Confirm" onPress={handleConfirm} disabled={isConfirmDisabled} color="#1E90FF" />
-                </View>
-            </View>
-            </View>
-        </Modal>
-    )
+          {message.length > 0 && <Text>{message}</Text>}
+
+          <View style={styles.buttonContainer}>
+            <Button title="Cancel" onPress={handleCancel} color="#1E90FF" />
+            <Button
+              title="Confirm"
+              onPress={handleConfirm}
+              disabled={isConfirmDisabled}
+              color="#1E90FF"
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    modalContent: {
-        width: '80%',
-        backgroundColor: '#fff',
-        padding: 20,
-        borderRadius: 10,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 3,
-        elevation: 5,  
-    },
-    input: {
-        borderBottomColor: "purple",
-        borderBottomWidth: 2,
-        width: '80%',
-        padding: 10,
-        fontSize: 16,
-        marginBottom: 20,
-    },
-    buttonContainer: {
-        flexDirection: 'row',//horizontally
-        justifyContent: 'space-between',
-        width: '60%',
-        marginTop: 20,
-    },
-    image: {
-        width: 100,
-        height: 100,
-        marginBottom: 20,
-    },
-    charCount: {
-        fontSize: 14,
-        color: '#000',
-        marginVertical: 10,
-    },
-    message: {
-        fontSize: 16,
-        color: 'red',
-        marginBottom: 20,
-    },
-   
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  input: {
+    borderBottomColor: "purple",
+    borderBottomWidth: 2,
+    width: "80%",
+    padding: 10,
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row", //horizontally
+    justifyContent: "space-between",
+    width: "60%",
+    marginTop: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  charCount: {
+    fontSize: 14,
+    color: "#000",
+    marginVertical: 10,
+  },
+  message: {
+    fontSize: 16,
+    color: "red",
+    marginBottom: 20,
+  },
 });
