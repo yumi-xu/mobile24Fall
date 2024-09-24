d4f02f0d2b32b1ed55c96c8ef7251237b853af9f
yumi-xu <happyleaf90@outlook.com>
2024-09-24 14:52:43 -0700

    clear the no needed code

--- App.js
+++ App.js
@@ -66,15 +66,6 @@ export default function App() {
           keyExtractor={(item) => item.id}
         />
       </View>
-      {/*<View style={styles.bottomSection}>*/}
-      {/*  <ScrollView contentContainerStyle={styles.contentContainer}>*/}
-      {/*    {goals.map((goal) => (*/}
-      {/*      <View style={styles.textWrapper} key={goal.id}>*/}
-      {/*        <Text style={styles.inputText}>{goal.text}</Text>*/}
-      {/*      </View>*/}
-      {/*    ))}*/}
-      {/*  </ScrollView>*/}
-      {/*</View>*/}
     </SafeAreaView>
   );
 }
@@ -118,25 +109,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingBottom: 20,
   },
-
-  // textWrapper: {
-  //   borderRadius: 5,
-  //   backgroundColor: "#E6E6FA",
-  //   margin: 10,
-  //   padding: 10,
-  //   borderWidth: 1,
-  //   borderColor: "#ccc",
-  //   shadowColor: "#000",
-  //   shadowOffset: { width: 0, height: 2 },
-  //   shadowOpacity: 0.8,
-  //   shadowRadius: 3,
-  //   elevation: 3,
-  // },
-  //
-  // inputText: {
-  //   fontSize: 18,
-  //   color: "black",
-  //   textAlign: "center",
-  //   borderRadius: 5
-  // },
 });
