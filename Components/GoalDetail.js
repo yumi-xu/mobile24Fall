c566521e09ece3d537a0e447cebaf77f819f620a
yumi-xu <happyleaf90@outlook.com>
2024-10-01 14:12:30 -0700

    Activity - More details

--- Components/GoalDetail.js
+++ Components/GoalDetail.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 const GoalDetails = ({ route }) => {
   const { goal } = route.params;
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
       <Text style={styles.detailText}>Goal Details:</Text>
       <Text style={styles.goalText}>Text: {goal.text}</Text>
       <Text style={styles.goalText}>ID: {goal.id}</Text>
+
+      <Button
+        title="More details"
+        onPress={() =>
+          navigation.push('Details', { goal: goal }) // Push a new GoalDetails screen onto the stack
+        }
+      />
     </View>
+
   );
 };
 
