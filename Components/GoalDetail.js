a22d850008578da2d4cb1ed9bd73b47c110e01fc
yumi-xu <happyleaf90@outlook.com>
2024-10-01 18:39:28 -0700

    format code

--- Components/GoalDetail.js
+++ Components/GoalDetail.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {useNavigation} from "@react-navigation/native";
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const GoalDetails = ({ route }) => {
   const navigation = useNavigation();
   console.log(route.params);
-  function moreDetailHandle(){
+  function moreDetailHandle() {
     navigation.push("Details");
   }
 
   return (
     <View style={styles.container}>
-      {route.params ?
-        (<Text style={styles.detailText}>Text: {route.params.goal.text} id :{route.params.goal.id}</Text>):
-        (<Text>More Details</Text>)
-      }
+      {route.params ? (
+        <Text style={styles.detailText}>
+          Text: {route.params.goal.text} id :{route.params.goal.id}
+        </Text>
+      ) : (
+        <Text>More Details</Text>
+      )}
 
-      <Button
-        title="More details"
-        onPress={moreDetailHandle}
-      />
+      <Button title="More details" onPress={moreDetailHandle} />
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f8f9fa',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f8f9fa",
   },
   detailText: {
     fontSize: 20,
     marginBottom: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
