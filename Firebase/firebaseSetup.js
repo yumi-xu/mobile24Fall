49af84f1e70ba582ac3cca50fe09b6bc137fcbf0
yumi-xu <happyleaf90@outlook.com>
2024-10-15 15:08:54 -0700

    use env to get the config

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -7,25 +7,25 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.EXPO_PUBLIC_apiKey,
-//   authDomain: process.env.EXPO_PUBLIC_authDomain,
-//   projectId: process.env.EXPO_PUBLIC_projectId,
-//   storageBucket: process.env.EXPO_PUBLIC_storageBucket,
-//   messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
-//   appId: process.env.EXPO_PUBLIC_appId,
-//   measurementId: process.env.EXPO_PUBLIC_measurementId,
-// };
-console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyDltuZqNe41tGmhplEMPasUseBPI8ZgU9c",
-  authDomain: "goals-app-e9259.firebaseapp.com",
-  projectId: "goals-app-e9259",
-  storageBucket: "goals-app-e9259.appspot.com",
-  messagingSenderId: "476893558988",
-  appId: "1:476893558988:web:665998d822fb2a45ec6a69",
-  measurementId: "G-PXLJ56P1BD",
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId,
+  measurementId: process.env.EXPO_PUBLIC_measurementId,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDltuZqNe41tGmhplEMPasUseBPI8ZgU9c",
+//   authDomain: "goals-app-e9259.firebaseapp.com",
+//   projectId: "goals-app-e9259",
+//   storageBucket: "goals-app-e9259.appspot.com",
+//   messagingSenderId: "476893558988",
+//   appId: "1:476893558988:web:665998d822fb2a45ec6a69",
+//   measurementId: "G-PXLJ56P1BD",
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
