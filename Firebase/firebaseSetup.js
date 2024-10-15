f44ed86007ebd99c35e51eef485a1ff65c092171
yumi-xu <happyleaf90@outlook.com>
2024-10-15 13:29:27 -0700

    Managing API keys for Firebase

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDltuZqNe41tGmhplEMPasUseBPI8ZgU9c",
-  authDomain: "goals-app-e9259.firebaseapp.com",
-  projectId: "goals-app-e9259",
-  storageBucket: "goals-app-e9259.appspot.com",
-  messagingSenderId: "476893558988",
-  appId: "1:476893558988:web:665998d822fb2a45ec6a69",
-  measurementId: "G-PXLJ56P1BD",
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId,
+  measurementId: process.env.EXPO_PUBLIC_measurementId,
 };
 
 // Initialize Firebase
