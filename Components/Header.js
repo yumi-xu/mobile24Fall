8e2590e8f5d0d24f6c144fc6f383eef85861f3e3
yumi-xu <happyleaf90@outlook.com>
2024-10-22 15:35:15 -0700

    Dimensions API

--- Components/Header.js
+++ Components/Header.js
@@ -1,4 +1,10 @@
-import { View, Text, Dimensions, useWindowDimensions } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
 import React from "react";
 
 const windowWidth = Dimensions.get("window").width;
@@ -7,12 +13,18 @@ const windowHeight = Dimensions.get("window").height;
 //uopdate this variable to change the app name
 export default function Header({ name }) {
   const { width, height } = useWindowDimensions();
+  const paddingVerticalDynamic = height < 415 ? 0 : 10;
   return (
     <View>
       <Text style={[style.text, { paddingVertical: height < 415 ? 0 : 10 }]}>
-        {" "}
         Welcome to {name}
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: "red",
+  },
+});
