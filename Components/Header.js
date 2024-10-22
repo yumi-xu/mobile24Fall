e9385d361b1ceda407102f5243f22d0db3d07bd6
yumi-xu <happyleaf90@outlook.com>
2024-10-22 15:06:16 -0700

    Dimensions API

--- Components/Header.js
+++ Components/Header.js
@@ -1,12 +1,18 @@
 import { View, Text, Dimensions, useWindowDimensions } from "react-native";
 import React from "react";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 //uopdate this variable to change the app name
 export default function Header({ name }) {
   const { width, height } = useWindowDimensions();
   return (
     <View>
-      <Text> Welcome to {name}</Text>
+      <Text style={[style.text, { paddingVertical: height < 415 ? 0 : 10 }]}>
+        {" "}
+        Welcome to {name}
+      </Text>
     </View>
   );
 }
