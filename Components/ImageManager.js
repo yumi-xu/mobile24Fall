bdd5ff64119b3055a1c4722a8a9b0e0245a691f4
yumi-xu <happyleaf90@outlook.com>
2024-11-05 14:25:01 -0800

    Activity  - Image Preview

--- Components/ImageManager.js
+++ Components/ImageManager.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { View, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { View, Button, Alert, Image } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImageManager() {
+  const [imageUri, setImageUri] = useState(null);
   const [response, requestPermission] = ImagePicker.useCameraPermissions();
   console.log(response);
 
@@ -35,6 +36,9 @@ export default function ImageManager() {
       // Check if the image was taken or the action was canceled
       if (!result.canceled) {
         console.log("Image captured:", result.assets[0].uri);
+        const uri = result.assets[0].uri;
+        console.log("Image captured:", uri);
+        setImageUri(uri);
       }
     } catch (err) {
       console.error("Error accessing the camera:", err);
@@ -45,7 +49,12 @@ export default function ImageManager() {
   return (
     <View>
       <Button title="Take An Image" onPress={takeImageHandler} />
-      <Image />
+      {imageUri && (
+        <Image
+          source={{ uri: imageUri }}
+          style={{ width: 200, height: 200, marginTop: 10 }} // 设置预览图像的样式
+        />
+      )}
     </View>
   );
 }
